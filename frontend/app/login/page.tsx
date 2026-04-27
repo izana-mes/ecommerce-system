561bd91ec77363ddff164114ed6e0fed4de13e14
izana-mes <nguyennguyenhuy357@gmail.com>
2026-04-27 10:33:57 +0700

    feat: wrap LoginSignUp component in Suspense to handle dynamic loading states

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,12 +1,12 @@
+import { Suspense } from "react";
 import LoginSignUp from "@/components/Authentication/LoginSign/LoginSignUp";
 
 export default function LoginPage() {
   return (
-    <>
+    <Suspense fallback={null}>
       <LoginSignUp />
-    </>
+    </Suspense>
   );
 }
-
 
 
